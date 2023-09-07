6542bfd332c70e1cd576d1d18c2e8d1e0c41f046
caushcani <caushcani@gmail.com>
2023-09-07 17:41:34 +0200

    fix: schema type

--- backend/src/middleware/protection/validateRequest.ts
+++ backend/src/middleware/protection/validateRequest.ts
@@ -1,13 +1,14 @@
-import { Request, Response, NextFunction } from 'express';
-const validateRequest = (schema) => {
-    return (req: Request, res:Response, next:NextFunction) => {
-      const { error } = schema.validate(req.body);
-      if (error) {
-        //status code is 400, because everytime this error is catched, its for a bad request reason
-        return res.status(400).json({ error: error.details[0].message });
-      }
-      next();
-    };
-}
+import { Request, Response, NextFunction } from "express";
+import { Schema } from "joi";
+const validateRequest = (schema: Schema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      //status code is 400, because everytime this error is catched, its for a bad request reason
+      return res.status(400).json({ error: error.details[0].message });
+    }
+    next();
+  };
+};
 
 export default validateRequest;
